cdf048f6c6407a0ec0419fa7314f2ad0f1514a09
Alessio Cuccovillo <alessio.cuccovillo@gmail.com>
2023-02-19 00:03:41 +0100

    Upgrade to JUnit 5

--- src/test/java/org/mozilla/universalchardet/BasicStreamEncodingDetectionTest.java
+++ src/test/java/org/mozilla/universalchardet/BasicStreamEncodingDetectionTest.java
@@ -1,88 +1,104 @@
 package org.mozilla.universalchardet;
 
+import org.junit.jupiter.api.Test;
+
 import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 
-import org.junit.Assert;
-import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-public class BasicStreamEncodingDetectionTest {
+class BasicStreamEncodingDetectionTest {
+
 	@Test
-	public void testUTF8 () throws IOException {
-		Assert.assertEquals("UTF-8", getFileEncoding("utf8.txt"));
+	void testUTF8() throws IOException {
+
+		assertEquals("UTF-8", getFileEncoding("utf8.txt"));
 	}
+
 	@Test
-	public void testUTF8N () throws IOException {
-		Assert.assertEquals("UTF-8", getFileEncoding("utf8n.txt"));
+	void testUTF8N() throws IOException {
+
+		assertEquals("UTF-8", getFileEncoding("utf8n.txt"));
 	}
+
 	@Test
-	public void testUTF16LE () throws IOException {
-		Assert.assertEquals("UTF-16LE", getFileEncoding("utf16le.txt"));
+	void testUTF16LE() throws IOException {
+
+		assertEquals("UTF-16LE", getFileEncoding("utf16le.txt"));
 	}
+
 	@Test
-	public void testShifJis () throws IOException {
-		Assert.assertEquals("SHIFT_JIS", getFileEncoding("shiftjis.txt"));
+	void testShiftJis() throws IOException {
+
+		assertEquals("SHIFT_JIS", getFileEncoding("shiftjis.txt"));
 	}
-	
+
 	@Test
-	public void testEUC () throws IOException {
-		Assert.assertEquals("EUC-JP", getFileEncoding("euc.txt"));
-	}	
-	@Test
-	public void testISO2022JP () throws IOException {
-		Assert.assertEquals("ISO-2022-JP", getFileEncoding("iso2022jp.txt"));
+	void testEUC() throws IOException {
+
+		assertEquals("EUC-JP", getFileEncoding("euc.txt"));
 	}
-	
+
 	@Test
-	public void testBIG5 () throws IOException {
-		Assert.assertEquals("BIG5", getFileEncoding("big5.txt"));
+	void testISO2022JP() throws IOException {
+
+		assertEquals("ISO-2022-JP", getFileEncoding("iso2022jp.txt"));
 	}
-	
+
 	@Test
-	public void testEUCTW () throws IOException {
-		Assert.assertEquals("EUC-TW", getFileEncoding("euctw.txt"));
+	void testBIG5() throws IOException {
+
+		assertEquals("BIG5", getFileEncoding("big5.txt"));
 	}
+
 	@Test
-	public void testEUCKR() throws IOException {
-		Assert.assertEquals("EUC-KR", getFileEncoding("euckr.txt"));
+	void testEUCTW() throws IOException {
+
+		assertEquals("EUC-TW", getFileEncoding("euctw.txt"));
 	}
+
 	@Test
-	public void testWindows1255 () throws IOException {
-		Assert.assertEquals("WINDOWS-1255", getFileEncoding("windows1255.txt"));
+	void testEUCKR() throws IOException {
+
+		assertEquals("EUC-KR", getFileEncoding("euckr.txt"));
 	}
-	
+
 	@Test
-	public void testUTF8Emoji () throws IOException {
-		Assert.assertEquals("UTF-8", getFileEncoding("utf8n-emoji.txt"));
+	void testWindows1255() throws IOException {
+
+		assertEquals("WINDOWS-1255", getFileEncoding("windows1255.txt"));
 	}
-	
-		 
-	
-	private String getFileEncoding(String testFileName) throws IOException{
-        File file = new File("src/test/resources/" + testFileName);
-        EncodingDetectorInputStream edis = null;
-        EncodingDetectorOutputStream edos = null;
-        try {
-        	edis = new EncodingDetectorInputStream(new BufferedInputStream(Files.newInputStream(file.toPath())));        
-	        edos = new EncodingDetectorOutputStream(NullOutputStream.NULL_OUTPUT_STREAM);
-	        byte[] buffer = new byte[1024];
-	        int read = 0;
-	        while (( read = edis.read(buffer)) > 0){
-	        	edos.write(buffer, 0, read);
-	        }
-        }
-        finally {
-        	edos.close();
-        	edis.close();
-        }
-        String encodingRead = edis.getDetectedCharset();
-        String encodingWrite = edos.getDetectedCharset();
-        Assert.assertNotNull(encodingRead);
-        Assert.assertNotNull(encodingWrite);
-        Assert.assertTrue(encodingRead.equals(encodingWrite));
-        return encodingRead;
-        
+
+	@Test
+	void testUTF8Emoji() throws IOException {
+
+		assertEquals("UTF-8", getFileEncoding("utf8n-emoji.txt"));
+	}
+
+	private String getFileEncoding(String testFileName) throws IOException {
+
+		File file = new File("src/test/resources/" + testFileName);
+
+		try (EncodingDetectorInputStream edis = new EncodingDetectorInputStream(
+				new BufferedInputStream(Files.newInputStream(file.toPath())));
+				EncodingDetectorOutputStream edos = new EncodingDetectorOutputStream(
+						NullOutputStream.NULL_OUTPUT_STREAM)) {
+			byte[] buffer = new byte[1024];
+			int read;
+			while ((read = edis.read(buffer)) > 0) {
+				edos.write(buffer, 0, read);
+			}
+			edis.close();
+			edos.close();
+			String encodingRead = edis.getDetectedCharset();
+			String encodingWrite = edos.getDetectedCharset();
+			assertNotNull(encodingRead);
+			assertNotNull(encodingWrite);
+			assertEquals(encodingRead, encodingWrite);
+			return encodingRead;
+		}
 	}
 }
